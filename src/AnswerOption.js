f00792d5bde5e79834a0d782fef0e668922ddcf7
avb5bt <81479081+avb5bt@users.noreply.github.com>
2022-04-22 10:38:19 -0400

    AnswerOption.js component

---
+++ src/AnswerOption.js
@@ -0,0 +1,9 @@
+function AnswerOption(props) {
+    return (
+      <p>
+        {props.info.food} (${props.info.price})//TODO: we need to figure out formatting of props
+      </p>
+    );
+  }
+  
+  export default Item;
